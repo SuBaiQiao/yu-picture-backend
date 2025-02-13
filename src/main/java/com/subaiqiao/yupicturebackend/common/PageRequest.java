32f035d6e3af702b195234ed6aeea158a2013c45
subaiqiao <1294749811@qq.com>
2025-02-13 13:03:32 +0800

    封装分页请求

--- src/main/java/com/subaiqiao/yupicturebackend/common/PageRequest.java
+++ src/main/java/com/subaiqiao/yupicturebackend/common/PageRequest.java
@@ -2,6 +2,27 @@ package com.subaiqiao.yupicturebackend.common;
 
 import lombok.Data;
 
+/**
+ * 分页对象
+ */
 @Data
 public class PageRequest {
+
+    /**
+     * 当前页码
+     */
+    private int current = 1;
+    /**
+     * 页面大小
+     */
+    private int pageSize = 10;
+    /**
+     * 排序字段
+     */
+    private String sortField;
+    /**
+     * 排序顺序（默认升序）
+     */
+    private String sortOrder = "descend";
+
 }
