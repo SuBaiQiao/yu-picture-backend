f4f9ac291998de419e586efafeb3f51ce5e01c22
SuBaiQiao <1294749811@qq.com>
2025-03-24 12:39:19 +0800

    空间用户管理

---
+++ src/main/java/com/subaiqiao/yupicturebackend/mapper/SpaceUserMapper.java
@@ -0,0 +1,19 @@
+package com.subaiqiao.yupicturebackend.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.subaiqiao.yupicturebackend.model.entity.SpaceUser;
+
+/**
+* @author Caozhaoyu
+* @description 针对表【space_user(空间用户关联)】的数据库操作Mapper
+* @createDate 2025-03-22 09:51:30
+* @Entity generator.domain.SpaceUser
+*/
+public interface SpaceUserMapper extends BaseMapper<SpaceUser> {
+
+}
+
+
+
+
